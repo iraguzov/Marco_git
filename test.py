52289e3c73ae69ed7af03c08d98c9e4260f40baa
iraguzov <raguzov.i.a@gmail.com>
2024-11-14 14:17:51 +0300

    updatedHeadlessTrue

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright
 def check_page_title():
     with sync_playwright() as p:
         # Launch a browser (Chrome in this case)
-        browser = p.chromium.launch(headless=False)  # headless=False to open the browser visibly
+        browser = p.chromium.launch(headless=True)  # headless=False to open the browser visibly
         page = browser.new_page()
         # Go to the specified URL
         page.goto("https://neuralms.com")
