c12c4608f9e2758fbdfcc6ac0f135b69eba550a0
iraguzov <raguzov.i.a@gmail.com>
2024-11-14 14:07:34 +0300

    new

--- test.py
+++ test.py
@@ -5,19 +5,17 @@ def check_page_title():
         # Launch a browser (Chrome in this case)
         browser = p.chromium.launch(headless=False)  # headless=False to open the browser visibly
         page = browser.new_page()
-
         # Go to the specified URL
         page.goto("https://neuralms.com")
-
         # Check the title of the page
         expected_title = "Neural MS Consulting - Home"
         actual_title = page.title()
-
         # Print result based on title match
         if actual_title == expected_title:
             print("pass")
         else:
             print("fail")
-
         # Close the browser
         browser.close()
+
+check_page_title()
